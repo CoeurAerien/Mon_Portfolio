f5dfd02c6aa88e66972027e61c9d05ca25607800
CoeurAerien <sekoudia34@gmail.com>
2025-06-26 22:26:25 +0200

    Ajout initial des fichiers du site web

---
+++ MyScripts.js
@@ -0,0 +1,5 @@
+function mafonction(){
+    //document.getElementById("demo").innerText = "Je me présente Sékou Mahamadou Zan Diarra, je suis étuidiant en BTSSIO option SLAM";
+    window.alert("Bienvenue sur mon portfolio");
+    console.log(5+8);
+}
